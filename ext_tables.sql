571e7c9e626ccbb6aa481d6a7ebdf4bf7e297e89
Alex Kellner <alexander.kellner@in2code.de>
2018-02-27 20:53:21 +0100

    [TASK] Start adding workflow module without any content

--- ext_tables.sql
+++ ext_tables.sql
@@ -156,3 +156,27 @@ CREATE TABLE tx_lux_domain_model_log (
 	KEY parent (pid),
 	KEY language (l10n_parent,sys_language_uid)
 );
+
+CREATE TABLE tx_lux_domain_model_workflow (
+	uid int(11) NOT NULL auto_increment,
+	pid int(11) DEFAULT '0' NOT NULL,
+
+	triggers text NOT NULL,
+	actions text NOT NULL,
+
+	title varchar(255) DEFAULT '' NOT NULL,
+	description varchar(255) DEFAULT '' NOT NULL,
+
+	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
+	crdate int(11) unsigned DEFAULT '0' NOT NULL,
+	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
+	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
+
+	sys_language_uid int(11) DEFAULT '0' NOT NULL,
+	l10n_parent int(11) DEFAULT '0' NOT NULL,
+	l10n_diffsource mediumblob,
+
+	PRIMARY KEY (uid),
+	KEY parent (pid),
+	KEY language (l10n_parent,sys_language_uid)
+);
