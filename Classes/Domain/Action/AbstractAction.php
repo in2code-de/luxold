2cfb0c73585539e75742341bc8ba76a8eca3e2b0
Alex Kellner <alexander.kellner@in2code.de>
2018-04-19 20:53:52 +0200

    [TASK] Fix small typo in code

--- Classes/Domain/Action/AbstractAction.php
+++ Classes/Domain/Action/AbstractAction.php
@@ -16,7 +16,7 @@ abstract class AbstractAction implements ActionInterface
 {
 
     /**
-     * Define and overwrite conrollter action on which your action should listen. Per default actions are only called
+     * Define and overwrite controller action on which your action should listen. Per default actions are only called
      * from pageRequestAction. In some cases (e.g. let's send an email on identification) it could be helpful to
      * also add another controller action as entry point.
      * Possible actions are:
